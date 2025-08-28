f27b9e9fc12beabfeb8e07df91a8d2c198d2d23c
aver <iamzoeyyy0.0@gmail.com>
2025-08-28 14:46:00 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,22 +8,24 @@ import schedule
 import time
 import datetime
 from pytz import timezone
+from keep_alive import keep_alive
+keep_alive()
 
 def get_client():
 
-  client = tweepy.Client(
+    client = tweepy.Client(
+        consumer_key=os.environ["CONSUMER_KEY"],
+        consumer_secret=os.environ["CONSUMER_SECRET"],
+        access_token=os.environ["ACCESS_TOKEN"],
+        access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
+    )
 
-    consumer_key=os.environ['CONSUMER_KEY'],
-    consumer_secret=os.environ['CONSUMER_SECRET'],
-    access_token=os.environ['ACCESS_TOKEN'],
-    access_token_secret=os.environ['ACCESS_TOKEN_SECRET'],
+    return client
 
-  )
-
-  return client
 
 def tweet(client, text):
-  client.create_tweet(text=text)
+    client.create_tweet(text=text)
+
 
 """
 
@@ -34,29 +36,32 @@ def tweet(client, text):
 
 strings = list()
 
-with open('text_data.json', 'r') as f:
-  print("Loading strings")
-  data = json.load(f)
-  for s in data['strings']:
-    string = re.sub(r"((\\|\^).[0-9]?)|(\/(.+)?)", "", s)
-    string = re.sub(r"&|#", "", string)
-  
-    if len(string) > 10:
-      strings.append(string)
+with open("text_data.json", "r") as f:
+    print("Loading strings")
+    data = json.load(f)
+    for s in data["strings"]:
+        string = re.sub(r"((\\|\^).[0-9]?)|(\/(.+)?)", "", s)
+        string = re.sub(r"&|#", "\n", string)
+
+        if len(string) > 10:
+            strings.append(string)
+
 
 def get_word():
-  return random.choice(strings)
+    return random.choice(strings)
+
 
 def tweet_quote():
-  client = get_client()
-  text = get_word()
-  tweet(client, text)
-  print("Job ran at:", datetime.datetime.now(timezone("Asia/Bangkok")))
+    client = get_client()
+    text = get_word()
+    tweet(client, text)
+    print("Job ran at:", datetime.datetime.now(timezone("Asia/Bangkok")))
+
 
 print("Scheduling twitter bot")
 tweet_quote() # Tweet once
-schedule.every().day.at("14:00", timezone("Asia/Bangkok")).do(tweet_quote)
+schedule.every().day.at("15:00", timezone("Asia/Bangkok")).do(tweet_quote)
 
 while True:
-  schedule.run_pending()
-  time.sleep(1)
+    schedule.run_pending()
+    time.sleep(1)
